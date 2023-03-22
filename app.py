2a95f70b34071f25ad3134ef691c9221222d9136
Bel <114267626+BA-BEL@users.noreply.github.com>
2023-03-22 14:37:29 +1100

    added polar chart with d3 dropdown functionality

--- app.py
+++ app.py
@@ -49,33 +49,36 @@ def home():
 def get_player_stats():
     
     session = Session(engine1)
-    playerquery = session.query(player_stats.Player,player_stats.Age,player_stats.Team,
+    playerquery = session.query(player_stats.id, player_stats.Player,player_stats.Age,player_stats.Team,
     player_stats.Position,player_stats.Games_played,player_stats.Minutes_played_per_game,player_stats.Points_per_game,
     player_stats.two_point_field_goal_percentage,player_stats.three_point_field_goal_percentage,
     player_stats.Effective_field_goal_percentage,player_stats.Free_throw_percentage,
     player_stats.Total_rebounds_per_game,player_stats.Assists_per_game,player_stats.Steals_per_game,
     player_stats.Blocks_per_game,player_stats.Turnovers_per_game,player_stats.Personal_fouls_per_game).all()
+
     players_dict_list = []
 
     for player in playerquery:
         player_dict = {}
-        player_dict['Name']=player[0]
-        player_dict['Age']=player[1]
-        player_dict['Team']=player[2]
-        player_dict['Position']=player[3]
-        player_dict['Played']=player[4]
-        player_dict['Minutes']=player[5]
-        player_dict['Points_pg']=player[6]
-        player_dict['2p%']=round(player[7],3)
-        player_dict['3p%']=round(player[8],3)
-        player_dict['Effective%']=round(player[9],3)
-        player_dict['Free%']=round(player[10],3)
-        player_dict['Rebounds']=player[11]
-        player_dict['Assists']=player[12]
-        player_dict['Steals']=player[13]
-        player_dict['Blocks']=player[14]
-        player_dict['Turnovers']=player[15]
-        player_dict['Fouls']=player[16]
+        player_dict["id"] = player[0]
+        player_dict['Name']=player[1]
+        player_dict['Age']=player[2]
+        player_dict['Team']=player[3]
+        player_dict['Position']=player[4]
+        player_dict['Played']=player[5]
+        player_dict['Minutes']=player[6]
+        player_dict['Points_pg']=player[7]
+        player_dict['2p%']=round(player[8],3)
+        player_dict['3p%']=round(player[9],3)
+        player_dict['Effective%']=round(player[10],3)
+        player_dict['Free%']=round(player[11],3)
+        player_dict['Rebounds']=player[12]
+        player_dict['Assists']=player[13]
+        player_dict['Steals']=player[14]
+        player_dict['Blocks']=player[15]
+        player_dict['Turnovers']=player[16]
+        player_dict['Fouls']=player[17]
+
         players_dict_list.append(player_dict)
 
     response = jsonify(players_dict_list)
--- static/js/players.js
+++ static/js/players.js
@@ -1,44 +1,52 @@
 // Get the endpoint
 const url = "http://127.0.0.1:8000/api/v1.0/players";
 
-// Declare playersdata variable
-var playersdata;
-
 // Initialize the function 
 function init() {
 
   // Use D3 to get the options for drop down Menu
   d3.json(url).then((data) => {
-    playersdata = data;  
-    console.log(data)
+    let playersdata = data.map(item => item.Name);
+    let playerids = data.map(item => item.id)
+    // console.log(playersdata)
+    // console.log(playerids)
+    // console.log(data)
     // fill drop down with data
-    fillDrowpDown(playersdata);
+    fillDrowpDown(playersdata, playerids, data);
 
   });
 };
 
-function fillDrowpDown(data){
-  let dropdownMenu = d3.select("#selDataset");
-    
-  // Set a variable for the players name
-  let playersname = data.map(obj => obj.Name);
-console.log(playersname);
+function fillDrowpDown(playersdata, playerids, data) {
   
-// Add  players name to dropdown menu
-  playersname.forEach((id) => {
+  // Set a variable for the players name
+  let names = playersdata;
+  let ids = playerids; 
+  // console.log(playersname);
+  
+  let dropdownMenu = d3.select("#selDataset");
+  
+  let option;
 
-      console.log(id);
+  // Add  players name to dropdown menu
+  for(let i = 0; i < ids.length; i++){
+    
+    option = dropdownMenu.append("option");
+    option_value = option.attr("value",ids[i]);
+    option_text = option.text(names[i]);
 
-      dropdownMenu.append("option")
-      .text(id)
-      .property("value",id);
-  });
+  }
+
+
+
+    // console.log(id);
 
   // Set the first player from the list
-  let player_one = playersname[0];
+  let player = names[0];
+
   // buildDashboard(player_one, data);
-  buildMetadata(player_one, data);
-  buildGaugeChart(player_one, data);
+  buildMetadata(player, data);
+  buildGaugeChart(player, data);
 }
 // Function that populates players metadata info
 function buildMetadata(player, data) {
@@ -59,12 +67,12 @@ function buildMetadata(player, data) {
 
   // Use Object.entries to add each key/value pair to the panel
 
-  Object.entries(valueData).forEach(([key,value]) => {
+  Object.entries(valueData).forEach(([key, value]) => {
 
-      // Log the individual key/value pairs as they are being appended to the metadata panel
-      console.log(key,value);
+    // Log the individual key/value pairs as they are being appended to the metadata panel
+    // console.log(key, value);
 
-      d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
+    d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
   });
 
 };
@@ -72,12 +80,12 @@ function buildMetadata(player, data) {
 
 // Function that builds the Gauge chart
 function buildGaugeChart(player, data) {
- 
+
   let playerinfo = data;
   // Filter based on the value of the name
   let value = playerinfo.find(result => result.Name == player);
-  console.log(value), value.Played;  
-  let freq=value.Played
+  console.log(value), value.Played;
+  let freq = value.Played
   let trace2 = [{
     domain: { x: [0, 1], y: [0, 1] },
     value: freq,
@@ -85,93 +93,173 @@ function buildGaugeChart(player, data) {
     type: "indicator",
     mode: "gauge+number",
     gauge: {
-    axis: { range: [null, 82] },
-    steps: [
-    { range: [0, 20], color: "darkblue",text:"benchwarmer" },
-    { range: [20, 40], color: "blue",text:"consistant" },
-    { range: [40, 60], color: "darkred",text:"core player" },
-    { range: [60, 82], color: "red",text:"main man!" }
-    ]
+      axis: { range: [null, 82] },
+      steps: [
+        { range: [0, 20], color: "darkblue", text: "benchwarmer" },
+        { range: [20, 40], color: "blue", text: "consistant" },
+        { range: [40, 60], color: "darkred", text: "core player" },
+        { range: [60, 82], color: "red", text: "main man!" }
+      ]
     }
   }];
-  
-    let layout = { width: 600, height: 450, paper_bgcolor: "transparent", font: { color: "white", family: "Arial" }, margin: { t: 0, b: 0 } };
-    Plotly.newPlot('gauge', trace2, layout);
+
+  let layout = { width: 600, height: 450, paper_bgcolor: "transparent", font: { color: "white", family: "Arial" }, margin: { t: 0, b: 0 } };
+  Plotly.newPlot('gauge', trace2, layout);
 
 }
 
-function polar(){
-  const data = {
-    labels: [
-      '2p%',
-      '3p%',
-      'Assists',
-      'Free%',
-      'Points_pg',
-      'Blocks',
-      'Fouls',
-      'Rebounds',
-      'Steals',
-      'Turnovers'
-      
-  
-    ],
-    datasets: [{
-      label: 'Offensive stats',
-      data: [65, 59, 90, 81, 56, 55, 40],
-      fill: true,
-      backgroundColor: 'rgba(255, 99, 132, 0.2)',
-      borderColor: 'rgb(255, 99, 132)',
-      pointBackgroundColor: 'rgb(255, 99, 132)',
-      pointHoverBorderColor: 'rgb(255, 99, 132)'
-    }, {
-      label: 'defensive stats',
-      data: [28, 48, 40, 19, 96, 27, 100],
-      fill: true,
-      backgroundColor: 'rgba(54, 162, 235, 0.2)',
-      borderColor: 'rgb(54, 162, 235)',
-      pointBackgroundColor: 'rgb(54, 162, 235)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgb(54, 162, 235)'
-    }]
-  };
-  
-  const config = {
-    type: 'polarArea',
-    data: data,
-    options: {
-      elements: {
-        line: {
-          borderWidth: 3
+function polar(value ) {
+
+  let index = parseInt(value);
+
+  d3.json(url).then(function (data) {
+
+    // assign values to object for indentified player (default id = index = 0) 
+    // let fg2pct = data[index].2p%;
+    // let fg3pct = data[index].3p%;
+    // let ast = data[index].Assists;
+    // let ftpct = data[index].Free%;
+    // let stl = data[index].Steals
+    // let blk = data[index].Blocks;
+    // let tov = data[index].Turnovers;
+    // let reb = data[index].Rebounds;
+    // let pf = data[index].Fouls;
+    // let pts = data[index].Points_pg;
+
+    let fg2pct = data[index]["2p%"];
+    let fg3pct = data[index]["3p%"];
+    let ast = data[index].Assists;
+    let ftpct = data[index]["Free%"];
+    let stl = data[index].Steals
+    let blk = data[index].Blocks;
+    let reb = data[index].Rebounds;
+    let pf = data[index].Fouls;
+
+    // create array for this
+    let datarray = [fg2pct, fg3pct, ast, ftpct, stl, blk, reb, pf];
+
+    let bgcolors = ["rgba(119, 192, 3, 0.7)", 
+                        "rgba(255, 181, 47, 0.7)", 
+                        "rgba(247, 247, 0, 0.7)",
+                        "rgba(255, 128, 0, 0.7)",
+                        "rgba(235, 0, 205, 0.7)",
+                        "rgba(0, 0, 255, 0.7)",
+                        "rgba(0, 184, 249, 0.7)",
+                        "rgba(255, 0, 0, 0.7)"                       
+                        ]
+
+    const data_to_plot = {
+      labels: [
+        'FG2%',
+        'FG3%',
+        'AST',
+        'FT%',
+        'STL',
+        'BLK',
+        'REB',
+        'PF'
+      ],
+      datasets: [
+        {
+          label: 'Player stats',
+          data: datarray,
+          fill: true,
+          backgroundColor: bgcolors,
+          borderColor: 'rgba(175, 175, 175, 0.5)',
+          pointBackgroundColor: 'rgb(255, 99, 132)',
+          pointHoverBorderColor: 'rgb(255, 99, 132)'
         }
-      }
-    },
-  };
-  
-  let ctx = document.getElementById("polar").getContext('2d');
-  
-  polarchart = new Chart(ctx, config);
-  
-  console.log("Plotted polar chart");
+      ]
+    };
+
+    const config = {
+      type: 'polarArea',
+      data: data_to_plot,
+      options: {
+        responsive: true,
+        maintainAspectRatio: true,
+        elements: {
+          line: {
+            borderWidth: 3
+          }
+        },
+        plugins:{
+          legend:{
+            position:"right",
+            align: "start",
+            labels:{color:"white"}
+
+          }
+
+        }
+      },
+    };
+
+    let ctx = document.getElementById("polar").getContext('2d');
+
+    polarchart = new Chart(ctx, config);
+
+    console.log("Plotted polar chart");
+  });
 }
 
+function polarUpdate(value) {
 
+  let index = parseInt(value);
+
+  d3.json(url).then(function (data) {
+
+    // assign values to object for indentified player (default id = index = 0) 
+    // let fg2pct = data[index].2p%;
+    // let fg3pct = data[index].3p%;
+    // let ast = data[index].Assists;
+    // let ftpct = data[index].Free%;
+    // let stl = data[index].Steals
+    // let blk = data[index].Blocks;
+    // let tov = data[index].Turnovers;
+    // let reb = data[index].Rebounds;
+    // let pf = data[index].Fouls;
+    // let pts = data[index].Points_pg;
+
+    let fg2pct = data[index]["2p%"];
+    let fg3pct = data[index]["3p%"];
+    let ast = data[index].Assists;
+    let ftpct = data[index]["Free%"];
+    let stl = data[index].Steals
+    let blk = data[index].Blocks;
+    let reb = data[index].Rebounds;
+    let pf = data[index].Fouls;
+
+    // create array for this
+    let datarray = [fg2pct, fg3pct, ast, ftpct, stl, blk, reb, pf];
+
+    polarchart.data.datasets[0].data = (datarray);
+    polarchart.update()
+  });
+}
 
 // Function that updates dashboard when sample is changed
-function optionChanged(value) { 
+function optionChanged(value) {
 
-  // Log the new value
-  console.log(value); 
+  // console.log(value);
 
-  // Plot with the new sample 
-
-  buildMetadata(value, playersdata);
-  buildGaugeChart(value, playersdata);
-
+  d3.json(url).then(function(data) {
+    console.log(data[value].id);
+    console.log("TEST", data);
+    let playersdata = data;
+    // let playerdata = data[value];
+    // Log the new value
+    // console.log(value);
+    // Plot with the new sample 
+    
+    buildMetadata(data[value].Name, playersdata);
+    buildGaugeChart(data[value].Name, playersdata);
+    polarUpdate(value);
+    
+  })
 };
 
 // Call the initialize function
 init();
 
-polar();
+polar(0);
